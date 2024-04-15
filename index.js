612b87f423432571cae3f65a2613c987174e3ba0
advika <advika.a@outlook.com>
2024-04-15 22:10:11 +0530

    modified db name

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 3000;
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
-  database: "agrinnovate",
+  database: "farmdb",
   password: "advi@1436",
   port: 5432,
 });
